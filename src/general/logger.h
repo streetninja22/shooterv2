b890bffadf8a4cc1ffbbe47c1600e9a245d79415
streetninja22 <streetninja22@gmail.com>
2019-06-19 22:34:04 -0500

    Beginnings of file access and logging

---
+++ src/general/logger.h
@@ -0,0 +1,17 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <string>
+#include <fstream>
+#include <ctime>
+#include "filesystem.h"
+
+static class Logger
+{
+    std::string m_logPath;
+
+
+};
+
+
+#endif
